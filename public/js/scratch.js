fcefb7aceda26926df045c1248d3938fbfe3c7fe
ad3m3r5 <contact@aarondemers.com>
2022-11-14 18:57:15 -0500

    improve code reusability

--- public/js/scratch.js
+++ public/js/scratch.js
@@ -1,11 +1,7 @@
 var objectClass = null, objectGroups = null;
 
 if (validTypes.includes(mapType)) {
-  if (mapType == 'countries' || mapType == 'states') {
-    objectClass = document.querySelector(`.${mapType}`);
-  } else {
-    objectClass = document.querySelector(`.entities`);
-  }
+  objectClass = document.querySelector(`.entities`);
   objectGroups = objectClass.querySelectorAll(':scope > g');
 }
 
@@ -26,11 +22,7 @@ async function clickObject(e) {
     url: ''
   };
 
-  if (mapType == 'countries' || mapType == 'states') {
-    object.code = e.target.closest(`.${mapType} > g`).id;
-  } else {
-    object.code = e.target.closest(`.entities > g`).id;
-  }
+  object.code = e.target.closest(`.entities > g`).id;
 
   // get name of object
   for (var key of Object.keys(objectList)) {
@@ -166,11 +158,7 @@ async function clickObject(e) {
       scratchedObjects = dataSet;
 
       if (validTypes.includes(mapType)) {
-        if (mapType == 'countries' || mapType == 'states') {
-          objectClass = document.querySelector(`.${mapType}`);
-        } else {
-          objectClass = document.querySelector(`.entities`);
-        }
+        objectClass = document.querySelector(`.entities`);
         objectGroups = objectClass.querySelectorAll(':scope > g');
       }
       renderScratched(objectGroups);
