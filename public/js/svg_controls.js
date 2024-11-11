e04962774ce444461088ba292f6d8c9d8527d89b
ad3m3r5 <ad3m3r5@gmail.com>
2024-11-10 22:11:52 -0500

    added fullscreen svg view

--- public/js/svg_controls.js
+++ public/js/svg_controls.js
@@ -1,6 +1,8 @@
 const { pan, zoom, getScale, setScale, resetScale } = svgPanZoomContainer;
+var isFullscreen = false;
 
-const container = document.getElementById('svg-container');
+var mapContainer = document.getElementById('map-container');
+const svgContainer = document.getElementById('svg-container');
 
 const options = {
   origin: {
@@ -11,16 +13,38 @@ const options = {
   maxScale: 2
 };
 
+function fullscreenView() {
+  if (!isFullscreen) {
+    if (mapContainer.requestFullscreen) {
+      mapContainer.requestFullscreen();
+    } else if (mapContainer.webkitRequestFullscreen) { /* Safari */
+      mapContainer.webkitRequestFullscreen();
+    } else if (mapContainer.msRequestFullscreen) { /* IE11 */
+      mapContainer.msRequestFullscreen();
+    }
+    isFullscreen = true;
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+    isFullscreen = false;
+  }
+}
+
 function resetView() {
-  resetScale(container);
+  resetScale(svgContainer);
 }
 
 function zoomIn() {
   let maxZoom = (window.location.pathname == '/map/world' ? 8 : 2);
-  zoom(container, 1.2, {
+  zoom(svgContainer, 1.2, {
     origin: {
-      clientX: container.clientWidth/2,
-      clientY: container.clientHeight/2,
+      clientX: svgContainer.clientWidth/2,
+      clientY: svgContainer.clientHeight/2,
     },
     minScale: 1,
     maxScale: maxZoom
@@ -29,10 +53,10 @@ function zoomIn() {
 
 function zoomOut() {
   let maxZoom = (window.location.pathname == '/map/world' ? 8 : 2);
-  zoom(container, 0.8, {
+  zoom(svgContainer, 0.8, {
     origin: {
-      clientX: container.clientWidth/2,
-      clientY: container.clientHeight/2,
+      clientX: svgContainer.clientWidth/2,
+      clientY: svgContainer.clientHeight/2,
     },
     minScale: 1,
     maxScale: maxZoom
