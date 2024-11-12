409293b54337b12ed44fb561150de4cc03385a2c
ad3m3r5 <ad3m3r5@gmail.com>
2024-11-12 00:27:51 -0500

    update globals to be more accepting

--- utils/globals.js
+++ utils/globals.js
@@ -9,16 +9,38 @@ global.ADDRESS = process.env.ADDRESS || '0.0.0.0';
 global.PORT = process.env.PORT || 3000;
 global.DATA_DIR = process.env.DATA_DIR || localDataDir;
 
-// Allow LOG_LEVEL to be any case
-global.LOG_LEVEL = (process.env.LOG_LEVEL).trim().toUpperCase() || 'INFO';
+// allow LOG_LEVEL to be any case
+if ('LOG_LEVEL' in process.env) {
+  // verbosity numbers based on syslog - https://en.wikipedia.org/wiki/Syslog
+  let logLevel = process.env.LOG_LEVEL.trim().toUpperCase();
+  if (logLevel === 'INFO' || logLevel === '6') {
+    global.LOG_LEVEL = 'INFO';
+  } else if (logLevel === 'DEBUG' || logLevel === '7') {
+    global.LOG_LEVEL = 'DEBUG';
+  }else {
+    console.error(`Invalid LOG_LEVEL provided [${process.env.LOG_LEVEL}], defaulting to INFO`);
+    global.LOG_LEVEL = 'INFO';
+  }
+} else {
+  global.LOG_LEVEL = 'INFO';
+}
 
-// Allow ENABLE_SHARE to be any case or 1
-if ('ENABLE_SHARE' in process.env && (process.env.ENABLE_SHARE.trim().toLowerCase() === 'true' || process.env.ENABLE_SHARE.trim() === '1')) {
-  global.ENABLE_SHARE = true;
+// allow ENABLE_SHARE to be any case or 1/0
+if ('ENABLE_SHARE' in process.env) {
+  let enableShare = process.env.ENABLE_SHARE.trim().toLowerCase();
+  if (enableShare === 'true' || enableShare === '1') {
+    global.ENABLE_SHARE = true;
+  } else if (enableShare === 'false' || enableShare === '0') {
+    global.ENABLE_SHARE = false;
+  } else {
+    console.error(`Invalid ENABLE_SHARE provided [${process.env.ENABLE_SHARE}], defaulting to false`);
+    global.ENABLE_SHARE = false;
+  }
 } else {
   global.ENABLE_SHARE = false;
 }
 
+// debug output
 if (global.LOG_LEVEL == 'DEBUG') {
   console.debug('globals.js:');
 
